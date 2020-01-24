232e8e182b394cfdbaae52999bffc74b2773aa87
Robert Salaiz <work.salaizrm@gmail.com>
2020-01-24 17:59:00 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,25 @@
 #write your code here
 
-def countdown
+def countdown (seconds)
+  countdown = 10
+  
+  while countdown > 0
+    puts "#{countdown} SECOND(S)!"
+    countdown -= 1
+  end
+  return "HAPPY NEW YEAR!"
+  
+end
+
+
+def countdown_with_sleep (seconds)
+  
+  while seconds > 0
+    puts "#{seconds} SECOND(S)!"
+    seconds -= 1
+    sleep(1)
+  end
+  return "HAPPY NEW YEAR!"
+  
+
 end
