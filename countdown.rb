711b4834853132cdd027e8b406d9b4dcd061b88f
Robert Salaiz <work.salaizrm@gmail.com>
2020-01-24 17:41:07 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,12 +1,16 @@
 #write your code here
 
 def countdown
-  countdown = 10
+countdown = 10
+
   while countdown < 10
-  puts "#{countdown}"
-  countdown -= 1
-end
-puts "HAPPY NEW YEAR!"
+    puts "#{countdown}"
+    countdown -= 1
+  end
+
+  if countdown == 0
+    puts "HAPPY NEW YEAR!"
+  end
   
   
 end
