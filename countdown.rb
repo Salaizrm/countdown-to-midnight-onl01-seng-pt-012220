95a95dfe4d946684725ab20f3c3e4797ed2d2824
Robert Salaiz <work.salaizrm@gmail.com>
2020-01-24 17:44:49 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,9 @@
 #write your code here
 
 def countdown (seconds)
-countdown = 10
-
-  while countdown < 10
+  countdown = 10
+  
+  while countdown <= 10
     puts "#{countdown} SECOND(S)"
     countdown -= 1
   end
